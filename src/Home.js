38c9c6b23f4edded783a94824d22b70fc3782db4
robochris <robochrisc@gmail.com>
2020-04-08 21:07:34 -0400

    Start
    Start of this repo

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class Home extends Component {
+  constructor(props){
+  super(props);
+  this.state={
+  }
+ }
+  return (
+    <div>
+      Home
+    </div>
+  );
+}
